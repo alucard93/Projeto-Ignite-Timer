a363ed35f9d231e409dde2c04c0df99ae6742664
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-01-22 22:41:11 -0300

    feat: Button
    estudo styled componente com ts

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,8 +1,16 @@
+import { Button } from "./components/Button/Button"
+
 function App() {
 
   return (
     <div className="App">
-      
+      <>
+        <Button variant="primary"/>
+        <Button variant="secondary"/>
+        <Button variant="sucess"/>
+        <Button variant="danger"/>
+        <Button />
+      </>
     </div>
   )
 }
