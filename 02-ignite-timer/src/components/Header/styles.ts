4570d0e7dc7e53baea9f23196117abde79d7215f
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-02-09 14:03:56 -0300

    Create styles.ts

---
+++ 02-ignite-timer/src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex; //padrão row
+  align-items: center; // row padrão -  quando linha centraliza na altura
+  justify-content: space-between; // totalmente direita e totalmente esquerda
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  a {
+    width: 3rem;
+    height: 3rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    color: ${(props) => props.theme['gray-100']};
+
+    border-top: 3px solid transparent; // a bor. é coloc.transp. p/ n dar aquele efeito de bord. n. ao colc. o h. e evitar mexer o i.
+    border-bottom: 3px solid transparent; // é colocado encima e embaixo para deixar centralizado
+
+    &:hover {
+      border-bottom: 3px solid ${(props) => props.theme['green-500']};
+    }
+
+    &.active {
+      color: ${(props) => props.theme['green-500']};
+    }
+  }
+`
