94ac1bdcf6045a706802dcd48443cd72de3300e8
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-02-09 14:01:48 -0300

    Revert "feat: Header"
    This reverts commit 25e673af13edb965f0c44c1c20bd457ea81600c9.

--- 02-ignite-timer/src/components/Header/Header.tsx
+++ 02-ignite-timer/src/components/Header/Header.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import { HeaderContainer } from './styles'
-import Logo from '../../assets/Logo.svg'
-import { Timer, Scroll } from 'phosphor-react'
-import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <HeaderContainer>
-      <img src={Logo} alt="" />
-      <nav>
-        <NavLink to="/" title="Timer">
-          <Timer size={24} />
-        </NavLink>
-        <NavLink to="/history" title="Histórico">
-          <Scroll size={24} />
-        </NavLink>
-      </nav>
-    </HeaderContainer>
+    <header>
+      <div>
+        <h1>Header</h1>
+      </div>
+    </header>
   )
 }
-
-
-// NavLink to={} usado para fazer a transição entre as páginas e ele adiciona uma classe active
-// olhar styles &.active para entender melhor
